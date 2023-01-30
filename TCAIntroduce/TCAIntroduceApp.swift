cf8c66df8a8da35c34ee57ed2693289ed098065f
woodycatliu <bestlifeforzhi@gmail.com>
2023-01-30 17:03:45 +0800

    Initial Commit

---
+++ TCAIntroduce/TCAIntroduceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAIntroduceApp.swift
+//  TCAIntroduce
+//
+//  Created by Woody Liu on 2023/1/30.
+//
+
+import SwiftUI
+
+@main
+struct TCAIntroduceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
